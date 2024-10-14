0c75f3fcede8479385499247181956495ee2655c
Edison <aalevenegass@gmail.com>
2024-10-14 19:53:47 -0300

    Creando componente para mostrar pokemones

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
 import "./globals.css";
 import Header from "./ui/header/Header";
+import Pokemons from "./ui/Pokemon/Pokemons";
 
 export const metadata = {
   title: "PokeApi",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout() {
   return (
     <html lang="en">
       <body className="bg-[#202124] flex flex-col items-center">
         <Header />
-        {children}
+        <Pokemons />
       </body>
     </html>
   );
---
+++ src/app/ui/Pokemon/Pokemons.jsx
@@ -0,0 +1,7 @@
+export default function Pokemons() {
+  return (
+    <section>
+      <h1>Pokemons</h1>
+    </section>
+  );
+}
