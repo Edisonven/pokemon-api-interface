5c756ea1fc6a1435e52d60a68a14ee12fdc487cb
Edison <aalevenegass@gmail.com>
2024-10-15 18:29:53 -0300

    Componetizando card de pokemones de home

--- src/app/ui/Pokemon/Pokemons.jsx
+++ src/app/ui/Pokemon/Pokemons.jsx
@@ -1,11 +1,10 @@
 "use client";
 import useFetchingPokemonData from "@/hooks/useFetchingPokemonData";
-import Image from "next/image";
 import Pagination from "../pagination/Pagination";
-import Link from "next/link";
 import SearchBar from "../searchBar/SearchBar";
 import { useContext } from "react";
 import { PokemonContext } from "@/context/PokemonContext";
+import PokemonHomeCard from "../cards/PokemonHomeCard";
 
 export default function Pokemons() {
   //desestructuración de los elementos definidos en el customHook del manejo de la llamada a la api
@@ -26,90 +25,10 @@ export default function Pokemons() {
             <div className="flex items-center justify-center flex-wrap gap-5">
               {pokemonFinded.length > 0
                 ? pokemonFinded.map((pokemon) => (
-                  <div
-                      className="rounded shadow-md p-3 flex w-[300px] justify-between bg-white"
-                      key={pokemon.id}
-                    >
-                      <div>
-                        <p className="text-slate-800 font-semibold">
-                          # {pokemon.id}
-                        </p>
-                        <Image
-                          width={100}
-                          height={100}
-                          className="w-full max-w-[300px]"
-                          src={
-                            pokemon?.sprites?.other?.["official-artwork"]
-                              ?.front_default
-                          }
-                          alt="pokemon logo"
-                        />
-                      </div>
-                      <div className="">
-                        <h1 className="text-slate-800 text-center font-medium text-lg">
-                          {pokemon.name.charAt(0).toUpperCase() +
-                            pokemon.name.slice(1)}
-                        </h1>
-                        <div className="h-[80%] flex flex-col justify-between">
-                          <div>
-                            {pokemon.types.map((type, index) => (
-                              <p className="text-slate-800" key={index}>
-                                {type.type.name}
-                              </p>
-                            ))}
-                          </div>
-                          <Link
-                            className="px-2 py-1 bg-red-500 rounded shadow text-gray-100 font-medium hover:bg-red-400"
-                            href={`/${pokemon.id}`}
-                          >
-                            Ver detalles
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
+                    <PokemonHomeCard pokemon={pokemon} />
                   ))
                 : pokemons.map((pokemon) => (
-                    <div
-                      className="rounded shadow-md p-3 flex w-[300px] justify-between bg-white"
-                      key={pokemon.id}
-                    >
-                      <div>
-                        <p className="text-slate-800 font-semibold">
-                          # {pokemon.id}
-                        </p>
-                        <Image
-                          width={100}
-                          height={100}
-                          className="w-full max-w-[300px]"
-                          src={
-                            pokemon?.sprites?.other?.["official-artwork"]
-                              ?.front_default
-                          }
-                          alt="pokemon logo"
-                        />
-                      </div>
-                      <div className="">
-                        <h1 className="text-slate-800 text-center font-medium text-lg">
-                          {pokemon.name.charAt(0).toUpperCase() +
-                            pokemon.name.slice(1)}
-                        </h1>
-                        <div className="h-[80%] flex flex-col justify-between">
-                          <div>
-                            {pokemon.types.map((type, index) => (
-                              <p className="text-slate-800" key={index}>
-                                {type.type.name}
-                              </p>
-                            ))}
-                          </div>
-                          <Link
-                            className="px-2 py-1 bg-red-500 rounded shadow text-gray-100 font-medium hover:bg-red-400"
-                            href={`/${pokemon.id}`}
-                          >
-                            Ver detalles
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
+                    <PokemonHomeCard pokemon={pokemon} />
                   ))}
             </div>
           )}
---
+++ src/app/ui/cards/PokemonHomeCard.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function PokemonHomeCard({ pokemon }) {
+  return (
+    <div
+      className="rounded shadow-md p-3 flex w-[300px] justify-between bg-white"
+      key={pokemon.id}
+    >
+      <div>
+        <p className="text-slate-800 font-semibold"># {pokemon.id}</p>
+        <Image
+          width={100}
+          height={100}
+          className="w-full max-w-[300px]"
+          src={pokemon?.sprites?.other?.["official-artwork"]?.front_default}
+          alt="pokemon logo"
+        />
+      </div>
+      <div className="">
+        <h1 className="text-slate-800 text-center font-medium text-lg">
+          {pokemon?.name?.charAt(0).toUpperCase() + pokemon?.name?.slice(1)}
+        </h1>
+        <div className="h-[80%] flex flex-col justify-between">
+          <div>
+            {pokemon?.types?.map((type, index) => (
+              <p className="text-slate-800" key={index}>
+                {type?.type?.name}
+              </p>
+            ))}
+          </div>
+          <Link
+            className="px-2 py-1 bg-red-500 rounded shadow text-gray-100 font-medium hover:bg-red-400"
+            href={`/${pokemon.id}`}
+          >
+            Ver detalles
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
