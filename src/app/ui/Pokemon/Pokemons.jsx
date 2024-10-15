07615880d11c574893905063cb111e389facfc04
Edison <aalevenegass@gmail.com>
2024-10-14 22:18:51 -0300

    Creando estructura básica para mostrar pokemones

--- src/app/ui/Pokemon/Pokemons.jsx
+++ src/app/ui/Pokemon/Pokemons.jsx
@@ -1,16 +1,43 @@
 "use client";
 import useFetchingPokemonData from "@/hooks/useFetchingPokemonData";
+import Image from "next/image";
+
 export default function Pokemons() {
   const { pokemons } = useFetchingPokemonData();
 
   console.log(pokemons);
 
   return (
-    <section>
-      <div>
+    <section className="max-w-[1400px]">
+      <div className="flex items-center justify-center flex-wrap gap-3">
         {pokemons.map((pokemon) => (
-          <div key={pokemon.id}>
-            <h1 className="text-gray-300">{pokemon.name}</h1>
+          <div
+            className="border rounded shadow p-3 flex w-[300px] justify-between"
+            key={pokemon.id}
+          >
+            <div className="">
+              <Image
+                width={100}
+                height={100}
+                className="w-full max-w-[300px]"
+                src={
+                  pokemon?.sprites?.other?.["official-artwork"]?.front_default
+                }
+                alt="pokemon logo"
+              />
+            </div>
+            <div>
+              <h1 className="text-gray-300 text-center font-medium text-lg">
+                {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+              </h1>
+              <div>
+                {pokemon.types.map((type, index) => (
+                  <p className="text-gray-300" key={index}>
+                    {type.type.name}
+                  </p>
+                ))}
+              </div>
+            </div>
           </div>
         ))}
       </div>
--- src/hooks/useFetchingPokemonData.jsx
+++ src/hooks/useFetchingPokemonData.jsx
@@ -5,34 +5,34 @@ const useFetchingPokemonData = () => {
   const [limit, setLimit] = useState(20);
   const [offSet, setOffSet] = useState(0);
 
-  const handleFetchPokemonData = async () => {
-    try {
-      const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offSet}`
-      );
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
-
-      const data = await response.json();
-
-      const promises = data.results.map(async (pokemon) => {
-        const response = await fetch(pokemon.url);
-        const data = await response.json();
-        return data;
-      });
-
-      const results = await Promise.all(promises);
-      setPokemons(results);
-      return results;
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
   useEffect(() => {
+    const handleFetchPokemonData = async () => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offSet}`
+        );
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message);
+        }
+
+        const data = await response.json();
+
+        const promises = data.results.map(async (pokemon) => {
+          const response = await fetch(pokemon.url);
+          const data = await response.json();
+          return data;
+        });
+
+        const results = await Promise.all(promises);
+        setPokemons(results);
+        return results;
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
     handleFetchPokemonData();
   }, []);
 
