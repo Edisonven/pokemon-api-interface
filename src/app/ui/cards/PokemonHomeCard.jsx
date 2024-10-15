43cd4fd349e501b801cc5f7ba14d59d0b5dcc978
Edison <aalevenegass@gmail.com>
2024-10-15 20:26:27 -0300

    Implementando lógica para color de fondo de card, según tipo de pókemon (sujeto a mejora de lógica)

--- src/app/ui/cards/PokemonHomeCard.jsx
+++ src/app/ui/cards/PokemonHomeCard.jsx
@@ -4,7 +4,45 @@ import Image from "next/image";
 export default function PokemonHomeCard({ pokemon }) {
   return (
     <div
-      className="rounded shadow-md p-3 flex sm:w-[300px] justify-between bg-white"
+      className={`rounded shadow-md p-3 flex sm:w-[300px] bg-white justify-between ${
+        pokemon?.types[0].type.name === "grass"
+          ? "bg-[#3BCB01]"
+          : pokemon?.types[0].type.name === "water"
+          ? "bg-[#638FF0]"
+          : pokemon?.types[0].type.name === "fire"
+          ? "bg-[#EE8031]"
+          : pokemon?.types[0].type.name === "bug"
+          ? "bg-[#9CC31F]"
+          : pokemon?.types[0].type.name === "normal"
+          ? "bg-[#ADAB8D]"
+          : pokemon?.types[0].type.name === "poison"
+          ? "bg-[#AE01CB]"
+          : pokemon?.types[0].type.name === "fairy"
+          ? "bg-[#F9A5D1]"
+          : pokemon?.types[0].type.name === "electric"
+          ? "bg-[#FFD525]"
+          : pokemon?.types[0].type.name === "ground"
+          ? "bg-[#E5BC61]"
+          : pokemon?.types[0].type.name === "fighting"
+          ? "bg-[#C80000]"
+          : pokemon?.types[0].type.name === "psychic"
+          ? "bg-[#FE509B]"
+          : pokemon?.types[0].type.name === "rock"
+          ? "bg-[#C49100]"
+          : pokemon?.types[0].type.name === "ghost"
+          ? "bg-[#663993]"
+          : pokemon?.types[0].type.name === "ice"
+          ? "bg-[#88DBEC]"
+          : pokemon?.types[0].type.name === "dragon"
+          ? "bg-[#6622EE]"
+          : pokemon?.types[0].type.name === "dark"
+          ? "bg-[#494949]"
+          : pokemon?.types[0].type.name === "steel"
+          ? "bg-[#A6A8C6]"
+          : pokemon?.types[0].type.name === "flying"
+          ? "bg-[#A890FE]"
+          : ""
+      }`}
       key={pokemon.id}
     >
       <div>
@@ -12,6 +50,7 @@ export default function PokemonHomeCard({ pokemon }) {
         <Image
           width={100}
           height={100}
+          style={{ filter: "drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.5))" }}
           className="w-full max-w-[300px]"
           src={pokemon?.sprites?.other?.["official-artwork"]?.front_default}
           alt="pokemon logo"
