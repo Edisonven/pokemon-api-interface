4cc2b89bc07b39e91f0f34494ec145fd4b96b218
Edison <aalevenegass@gmail.com>
2024-10-15 21:17:09 -0300

    Asignando colores en card detalle de pokemon

--- src/app/ui/cards/PokemonDetailCard.jsx
+++ src/app/ui/cards/PokemonDetailCard.jsx
@@ -12,17 +12,17 @@ export default function PokemonDetailCard({ pokemonDetail }) {
       className={`border border-gray-100 shadow-lg rounded-2xl px-5 py-1 sm:w-[430px] ${backgroundColor}`}
     >
       <Link href="/" className="">
-        <IoIosArrowRoundBack className="text-slate-800 text-[40px] hover:bg-[#00000013] rounded-lg" />
+        <IoIosArrowRoundBack className="text-gray-100 text-[40px] hover:bg-[#00000013] rounded-lg" />
       </Link>
-      <h1 className="text-slate-800 font-semibold text-2xl mb-5">
+      <h1 className="text-gray-100 font-semibold text-2xl mb-5">
         {pokemonDetail.name.charAt(0).toUpperCase() +
           pokemonDetail.name.slice(1)}
       </h1>
       <div className="my-2 flex items-center gap-3">
-        <p className="font-semibold">Types:</p>
+        <p className="font-semibold text-gray-100">Types:</p>
         {pokemonDetail.types.map((type, index) => (
           <p
-            className="text-slate-800 font-medium border border-gray-300 pb-1 shadow rounded-3xl px-2 w-max flex"
+            className="text-gray-100 font-medium border border-gray-300 pb-1 shadow rounded-3xl px-2 w-max flex"
             key={index}
           >
             {type.type.name}
@@ -30,14 +30,16 @@ export default function PokemonDetailCard({ pokemonDetail }) {
         ))}
       </div>
       <div>
-        <p className="font-semibold mb-2">Stats:</p>
+        <p className="font-semibold mb-2 text-gray-100">Stats:</p>
         <div className="flex flex-col gap-2">
           {pokemonDetail.stats.map((stat, index) => (
             <div
               key={index}
               className="flex items-center justify-between  border-gray-300 border-b-2"
             >
-              <p className="font-medium">{stat?.stat?.name.toUpperCase()}</p>
+              <p className="font-medium text-gray-100">
+                {stat?.stat?.name.toUpperCase()}
+              </p>
               <p className="font-bold text-red-700">{stat?.base_stat}</p>
             </div>
           ))}
